a92c3649d4754632655a827c4fae6e510b8ce2d0
Beastman Kojak <beastman.kojak@gmail.com>
2022-06-14 23:38:42 -0400

    Fix framerate

--- bin/wave.sh
+++ bin/wave.sh
@@ -10,7 +10,7 @@ ls "$wave" | xargs -I {} composite "$wave/{}" \( "$baseDir/$baseFile" -resize 45
 if [ "$extension" = "gif" ]; then
   #ffmpeg -y -f image2 -framerate 8 -i "$baseDir/wave-%02d.png" "$baseDir/wave.gif"
   ffmpeg -y -f image2 -i "$baseDir/wave-%02d.png" -vf palettegen "$baseDir/palette.png"
-  ffmpeg -y -f image2 -thread_queue_size 16 -i "$baseDir/wave-%02d.png" -i "$baseDir/palette.png" -filter_complex "fps=8, paletteuse=dither=none" "$baseDir/wave.gif"
+  ffmpeg -y -f image2 -framerate 8 -thread_queue_size 16 -i "$baseDir/wave-%02d.png" -i "$baseDir/palette.png" -filter_complex "paletteuse=dither=none" "$baseDir/wave.gif"
 else
   ffmpeg -y -f image2 -framerate 8 -i "$baseDir/wave-%02d.png" -f apng -plays 0 "$baseDir/wave.apng"
 fi
