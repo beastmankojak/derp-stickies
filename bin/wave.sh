1ec4125a656057a4481e2c365b6a3c2cdc017edb
Beastman Kojak <beastman.kojak@gmail.com>
2022-06-14 23:17:41 -0400

    No dithering

--- bin/wave.sh
+++ bin/wave.sh
@@ -8,8 +8,10 @@ extension="$4"
 
 ls "$wave" | xargs -I {} composite "$wave/{}" \( "$baseDir/$baseFile" -resize 45% \) -gravity south -geometry +$offset+0 -resize 320x320 "$baseDir/{}"
 if [ "$extension" = "gif" ]; then
-  ffmpeg -y -f image2 -framerate 8 -i "$baseDir/wave-%02d.png" "$baseDir/wave.gif"
+  #ffmpeg -y -f image2 -framerate 8 -i "$baseDir/wave-%02d.png" "$baseDir/wave.gif"
+  ffmpeg -y -f image2 -i "$baseDir/wave-%02d.png" -vf palettegen "$baseDir/palette.png"
+  ffmpeg -y -f image2 -thread_queue_size 16 -i "$baseDir/wave-%02d.png" -i "$baseDir/palette.png" -filter_complex "fps=8, paletteuse=dither=none" "$baseDir/wave.gif"
 else
   ffmpeg -y -f image2 -framerate 8 -i "$baseDir/wave-%02d.png" -f apng -plays 0 "$baseDir/wave.apng"
 fi
-rm "$baseDir"/wave-*.png
+rm "$baseDir"/wave-*.png "$baseDir"/palette.png
